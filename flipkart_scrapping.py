55dbe0cf6ecc40348afd81c3b35a54774ffc9bb9
H K vamshidhar <vamshi.analyst2013@gmail.com>
2022-12-25 21:53:40 +0530

    Update flipkart_scrapping.py

--- flipkart_scrapping.py
+++ flipkart_scrapping.py
@@ -18,7 +18,7 @@ from bs4 import BeautifulSoup as bs
 from datetime import datetime
 from pytz import timezone
 
-path = '/content/solid-hope-363110-1c5cd2cd25cf.json'
+path = 'solid-hope-363110-1c5cd2cd25cf.json'
 sheet_id = '16-1AO0iPFTj6FlQpqmT9UmrhHqPrFaXwGVhWtcfzK7w'
 URL = "https://www.flipkart.com/google-pixel-6a-charcoal-128-gb/p/itme5ae89135d44e?pid=MOBGFKX5YUXD74Z3&lid=LSTMOBGFKX5YUXD74Z3MXA2OB&marketplace=FLIPKART&q=google+pixel+6a&store=tyy%2F4io&srno=s_1_1&otracker=AS_QueryStore_OrganicAutoSuggest_2_12_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_2_12_na_na_na&fm=organic&iid=a3b929d6-28cb-496d-97f8-f56a8096363d.MOBGFKX5YUXD74Z3.SEARCH&ppt=hp&ppn=homepage&ssid=86u2t4xoxc0000001671979563355&qH=02ccfad575fc2cbe"
 gc = pygsheets.authorize(service_account_file = path)
